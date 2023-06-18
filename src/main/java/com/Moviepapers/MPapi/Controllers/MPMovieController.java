b6ad2b14a59723cdbbd99c853dd878386024732b
deepak belamana <saideepak627@gmail.com>
2023-06-18 23:24:26 +0530

    'getAllMovieNamesAsList' method in MPMovieController will now return string of movie names

--- src/main/java/com/Moviepapers/MPapi/Controllers/MPMovieController.java
+++ src/main/java/com/Moviepapers/MPapi/Controllers/MPMovieController.java
@@ -1,7 +1,6 @@
 package com.Moviepapers.MPapi.Controllers;
 
 import com.Moviepapers.MPapi.Repositories.MPMovieRespository;
-import com.Moviepapers.MPapi.models.MPMovie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +14,7 @@ public class MPMovieController {
     MPMovieRespository MPMovieRespository;
 
     @GetMapping("/moviesList")
-    public List<MPMovie> getAllMovieNamesAsList() {
-       return MPMovieRespository.findAll();
+    public List<String> getAllMovieNamesAsList() {
+       return MPMovieRespository.findAll().stream().map(mpMovie -> mpMovie.getMovie()).toList();
     }
 }
