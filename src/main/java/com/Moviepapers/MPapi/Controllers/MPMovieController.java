7e1d07787c0b2ed967f494281075fc596bb62f04
deepak belamana <saideepak627@gmail.com>
2023-06-19 23:26:34 +0530

    Fixed CORS error by adding @CrossOrigin annotation for controller

--- src/main/java/com/Moviepapers/MPapi/Controllers/MPMovieController.java
+++ src/main/java/com/Moviepapers/MPapi/Controllers/MPMovieController.java
@@ -2,12 +2,14 @@ package com.Moviepapers.MPapi.Controllers;
 
 import com.Moviepapers.MPapi.Repositories.MPMovieRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class MPMovieController {
 
     @Autowired
